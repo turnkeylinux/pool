e82dad3de6542a20f3987cd89bbbd6f8a935b6dd
Liraz Siri <liraz@turnkeylinux.org>
2010-09-07 02:56:13 +0300

    print near fatal error on exception in `init' command

--- pylib/cmd_info.py
+++ pylib/cmd_info.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 """Prints list of registered stocks"""
+import sys
 import pool
 
+def fatal(s):
+    print >> sys.stderr, "error: " + str(s)
+    sys.exit(1)
+
 def main():
-    pool.Pool().print_info()
+    try:
+        pool.Pool().print_info()
+    except pool.Error, e:
+        fatal(e)
 
 if __name__ == "__main__":
     main()
