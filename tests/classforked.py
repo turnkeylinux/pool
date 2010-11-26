4c730c377bcd776ae7cfef8f0fd66bcfd0548fac
Liraz Siri <liraz@turnkeylinux.org>
2010-11-26 09:58:29 +0200

    tests/classforked: implemented generic method proxying + keywords passing

--- tests/classforked.py
+++ tests/classforked.py
@@ -3,12 +3,21 @@ import sys
 
 import pickle
 
+class FooError(Exception):
+    pass
+
 class _Foo:
     def getuid(self):
         return os.getuid()
 
     def getpid(self):
         return os.getpid()
+
+    def add(self, a, b):
+        return a + b
+
+    def makedict(self, name, age):
+        return {'name': name, 'age': 26}
 
 class Error(Exception):
     pass
@@ -19,17 +28,30 @@ class Pipe:
         self.r = os.fdopen(r, "r", 0)
         self.w = os.fdopen(w, "w", 0)
 
-class FooProxy:
+import new
+class ProxyInstance:
+    """This proxy class only proxies method invocations - no attributes"""
     def __init__(self, r, w):
         self.r = r
         self.w = w
 
-    def getpid(self):
-        pickle.dump(('getpid', ()), self.w)
-        error, val = pickle.load(self.r)
-        if error:
-            raise val
-        return val
+    @staticmethod
+    def _proxy(attrname):
+        def method(self, *args, **kws):
+            pickle.dump((attrname, args, kws), self.w)
+            error, val = pickle.load(self.r)
+            if error:
+                raise val
+            return val
+        return method
+
+    def __getattr__(self, name):
+        print "GETATTR"
+        unbound_method = self._proxy(name)
+        method = new.instancemethod(unbound_method,
+                                    self, self.__class__)
+        setattr(self, name, method)
+        return method
 
 def forkpipe():
     """Forks and create a bi-directional pipe -> (pid, r, w)"""
@@ -54,27 +76,31 @@ def Foo():
         obj = _Foo()
         while True:
             try:
-                attrname, args = pickle.load(r)
+                attrname, args, kws = pickle.load(r)
             except EOFError:
                 break
 
-            attr = getattr(obj, attrname)
-            if not callable(attr):
-                raise Error("'%s' is not callable" % attrname)
-
             try:
-                ret = attr(*args)
+                attr = getattr(obj, attrname)
+                if not callable(attr):
+                    raise Error("'%s' is not callable" % attrname)
+
+                ret = attr(*args, **kws)
                 pickle.dump((False, ret), w)
             except Exception, e:
                 pickle.dump((True, e), w)
 
         sys.exit(0)
 
-    return FooProxy(r, w)
-                    
+    return ProxyInstance(r, w)
         
 print "caller pid: %d" % os.getpid()
 foo = Foo()
+print "1 + 1 = %d" % foo.add(1, 1)
+print "dict: " + `foo.makedict("liraz", age=26)`
+
 print "foo pid: %d" % foo.getpid()
+print "foo pid: %d" % foo.getpid()
+print "foo uid: %d" % foo.getuid()
 
     
