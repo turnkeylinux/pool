0bea22a4331b46147554de0bac89a9f94356aa44
Alon Swartz <alon@turnkeylinux.org>
2012-12-05 13:06:50 +0200

    updated copyright/license

--- tests/regtest.sh
+++ tests/regtest.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# Copyright (c) TurnKey Linux - http://www.turnkeylinux.org
+#
+# This file is part of Pool
+#
+# Pool is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
 
 usage() {
     echo "syntax: $0 [ --options ]"
