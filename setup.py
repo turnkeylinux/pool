2dfa98b71556867d5ac1c66d37641e54155eea43
Jeremy Davis <jeremy@turnkeylinux.org>
2020-09-19 09:41:24 +1000

    Update debian/rules to use pybuild and add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(
+    name="pool",
+    version="2.0rc1",
+    author="Jeremy Davis",
+    author_email="jeremy@turnkeylinux.org",
+    url="https://github.com/turnkeylinux/pool",
+    packages=["pool_lib"],
+    scripts=["pool"]
+)
+
